5d37ec75c724dd4ba30c09956c44f525c7390572
Olusola Afikode <afikodeolaitan@gmail.com>
2023-03-21 19:25:20 +0100

    add enviroment variable

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const url = import.meta.env.VITE_APP_URL;
+
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://run.mocky.io/v3" }),
+  baseQuery: fetchBaseQuery({ baseUrl: url }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "7f02819f-8254-410a-b8af-ab98572bd26b",
